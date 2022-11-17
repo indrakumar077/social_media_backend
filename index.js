9b22f5e104280bfde7b55165c9cff636a3eef0ec
indrakumar077 <84073920+indrakumar077@users.noreply.github.com>
2022-11-17 10:37:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+import express, { response }  from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import AuthRoute from "./Routes/AuthRoute.js";  
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js"
+import cors from "cors";
+import UploadRoute from "./Routes/UploadRoute.js"
+import cloudinary from "./utill/cloudinary.js";
+
+const app = express();
+
+app.use(express.static('public'))
+app.use('/images',express.static("images"))
+
+app.use(bodyParser.json({limit:'30mb',extended:true}))
+
+app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
+app.use(cors());
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_DB,{useNewUrlParser:true, useUnifiedTopology:true}).then(()=>app.listen(process.env.PORT,()=>{
+    console.log(`connected to ${process.env.PORT}`);
+})).catch((e)=>console.log(e));
+
+app.use('/api/upload/image', async(req,res)=>{
+    
+    try {
+            const fileStr = req.body.data;
+            const uploadResponce = await cloudinary.uploader.upload(fileStr,{
+                upload_preset:'Social_media'
+            })
+            
+            console.log(uploadResponce.url);
+            res.json({url:uploadResponce.url});
+            
+        } catch (error) {
+            res.status(400).send("not good");
+        }
+
+
+})
+app.use('/auth',AuthRoute);
+app.use('/user',UserRoute);
+app.use('/post', PostRoute)
+app.use('/upload', UploadRoute)
