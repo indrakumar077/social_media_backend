0b77fb86bdb6eddd132fda97ff9774e88a3fc35f
indrakumar077 <84073920+indrakumar077@users.noreply.github.com>
2022-11-18 11:27:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use(cors());
 dotenv.config();
 
- const port = process.env.PORT || 3000
+ const port = process.env.PORT
 app.listen(port,()=>{
     console.log(`connected to ${process.env.PORT}`);
 })
